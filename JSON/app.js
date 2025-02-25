baaedab82d953f0b71fa173dbd618e81617226f9
KS MANOJ KUMAR <ksmk2104@gmail.com>
2025-02-25 21:18:01 +0530

    learnt about Routing and JSON data basics

--- JSON/app.js
+++ JSON/app.js
@@ -5,8 +5,11 @@ let html = fs.readFileSync("index.html","utf-8", (err)=>{
     if(err) {
         console.log(err);
     }
-})
+});
 
+let products = JSON.parse(fs.readFileSync("products.json","utf-8")); //Convert JSON Data into JSON Object
+
+let productListHtMl = fs.readFileSync("product-list.html","utf-8");
 
 const server = http.createServer( (request, response) => {
     let path = request.url;
@@ -14,7 +17,7 @@ const server = http.createServer( (request, response) => {
         response.writeHead(200,{
             'Content-Type': 'text/html',
         });
-        response.end(html.replace("{{%content%}}","You are in Home Page"));
+        response.end(html.replace("{{%content%}}",productListHtMl));
     }
     else if(path.toLocaleLowerCase() === '/contact'){
         response.writeHead(200,{
@@ -22,12 +25,13 @@ const server = http.createServer( (request, response) => {
         });
         response.end(html.replace("{{%content%}}","You are in Contact Page"));
     }
-    else if(path.toLocaleLowerCase() === '/about'){
+    else if(path.toLocaleLowerCase() === '/products'){
         response.writeHead(200,{
-            'Content-Type': 'text/html',
-            'my-header' : "I am OpThunder"
+            'Content-Type': 'application/json'
         });
-        response.end(html.replace("{{%content%}}","You are in About Page"));
+
+        response.end(JSON.stringify(products));
+        //console.log(products);
     }
     else if(path.toLocaleLowerCase() === '/services'){
         response.writeHead(200,{
@@ -45,5 +49,5 @@ const server = http.createServer( (request, response) => {
 
 server.listen( 8000, "127.0.0.1", ()=>{
     console.log('Server is running on port 8000');
-})
+});
 
